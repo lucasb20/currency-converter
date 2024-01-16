d6af3e016a4484e847f6635835fc25cc680b562f
Lucas Rocha <lr359508@gmail.com>
2024-01-16 15:43:47 -0300

    Finalização da API
    - Endpoint currency/converter deletado por conta do acesso limitado a API do fixer.io.
    
    - Há apenas o endpoint currency, que retorna o timestamp, moedas e seus valores relativos em float.

--- backend/Controllers/BackendController.cs
+++ backend/Controllers/BackendController.cs
@@ -7,7 +7,7 @@ namespace Backend.Controllers;
 [Route("currency")]
 public class CurrencyController : ControllerBase
 {
-    [HttpGet("flags")]
+    [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetCurrencyList(){
@@ -21,49 +21,26 @@ public class CurrencyController : ControllerBase
         var request = await client.GetStringAsync(fixer_url);
 
         List<string> symbols = [];
+        Dictionary<string, float> values = [];
+        long timestamp = 0;
 
         using (JsonDocument document = JsonDocument.Parse(request))
         {
             JsonElement root = document.RootElement;
-            JsonElement baseSymbol = root.GetProperty("base");
-            symbols.Add(baseSymbol.ToString());
+            timestamp = root.GetProperty("timestamp").GetInt64();
             JsonElement rates = root.GetProperty("rates");
             foreach(JsonProperty otherSymbol in rates.EnumerateObject()){
                 symbols.Add(otherSymbol.Name);
+                values.Add(otherSymbol.Name, (float) otherSymbol.Value.GetDouble());
             }
         }
 
         var json = new {
-            symbols = symbols
+            timestamp = timestamp,
+            symbols = symbols,
+            values = values
         };
 
         return new JsonResult(json);
-    }
-
-    [HttpPost("convert")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> GetConvert([FromBody] JsonElement data){
-         if (data.TryGetProperty("source", out var source))
-            {
-                HttpClient client = new();
-
-                string? accessKey = Environment.GetEnvironmentVariable("ACCESS_KEY");
-
-                string fixer_url = "http://data.fixer.io/api/latest" + "?access_key="+ accessKey+"&base="+source.ToString();
-
-                var request = await client.GetStringAsync(fixer_url);
-
-                using (JsonDocument document = JsonDocument.Parse(request))
-                {
-                    JsonElement root = document.RootElement;
-                    JsonElement rates = root.GetProperty("rates");
-                    return new JsonResult(rates);
-                }
-            }
-            else
-            {
-                return BadRequest("O JSON deve conter as chaves 'source' e 'destiny'.");
-            }
     }
 }
